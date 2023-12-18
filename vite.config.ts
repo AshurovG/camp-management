b7a1ca1cc0c36aa37061f8d123feed531830f5ef
Ilya Kulagin <ilya.kulagin@kaspersky.com>
2023-12-18 13:29:24 +0300

    Return base path

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ const parseTsConfigPaths = (paths: Record<string, string[]>): Record<string, str
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  base: "",
+  base: "camp-management",
   plugins: [react()],
   resolve: {
     alias: parseTsConfigPaths(tsconfig.compilerOptions.paths),
